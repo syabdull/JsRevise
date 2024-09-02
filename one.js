fde9091a5acf2f0c845875c582602458ad46817e
syabdull <syab5050@gmail.com>
2024-09-02 14:05:40 +0530

    print digits

--- one.js
+++ one.js
@@ -1,15 +1,25 @@
-// print n fibobacci numbers
+//print digits of a number from  left to right
 
-function fibonacciNumbers(n) {
-  let a = 0;
-  let b = 1;
-  let c;
-  for (let i = 1; i <= n; i++) {
-    console.log(a);
-    c = a + b;
-    a = b;
-    b = c;
+let num = 223223332;
+
+function printDigits(num) {
+  //findin the count of digits
+
+  let temp = num;
+  let count = 0;
+  while (temp != 0) {
+    temp = Math.round(temp / 10);
+    count++;
+  }
+  console.log("count", count);
+  let dsr = Math.pow(10, count - 1);
+  console.log("dsr", dsr);
+
+  while (dsr != 0) {
+    console.log(Math.floor(num / dsr));
+    num = num % dsr;
+    dsr = Math.floor(dsr / 10);
   }
 }
 
-fibonacciNumbers(5);
+printDigits(3344334);
