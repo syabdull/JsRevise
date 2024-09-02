eccf8e55e29256ece1fadede39b1e0ebd6c7a606
syabdull <syab5050@gmail.com>
2024-09-02 14:33:37 +0530

    reverse of a number

--- one.js
+++ one.js
@@ -1,25 +1,11 @@
-//print digits of a number from  left to right
+//js program to print the reverse of a number
 
-let num = 223223332;
-
-function printDigits(num) {
-  //findin the count of digits
-
-  let temp = num;
-  let count = 0;
-  while (temp != 0) {
-    temp = Math.round(temp / 10);
-    count++;
-  }
-  console.log("count", count);
-  let dsr = Math.pow(10, count - 1);
-  console.log("dsr", dsr);
-
-  while (dsr != 0) {
-    console.log(Math.floor(num / dsr));
-    num = num % dsr;
-    dsr = Math.floor(dsr / 10);
+function reverseOfNumber(num) {
+  while (num != 0) {
+    let rem = num % 10;
+    console.log(rem);
+    num = Math.floor(num / 10);
   }
 }
 
-printDigits(3344334);
+reverseOfNumber(566442);
